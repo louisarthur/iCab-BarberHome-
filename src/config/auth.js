4e5ae9873f011e242101ad045600802fe5810531
Louis Arthur <louisartur.53@gmail.com>
2019-08-17 23:16:57 -0300

    backend do icab, com o mongodb instalado

--- src/config/auth.js
+++
@@ -1,4 +0,0 @@
-export default {
-  secret: 'c3d5a03851b602a29ab6825a79e499bd',
-  expiresIn: '7d',
-};
--- src/server.js
+++
@@ -1,6 +0,0 @@
-import app from './app';
-
-const porta = 3333;
-/* colocando para ouvir executar o app na porta 3333 */
-
-app.listen(porta);
